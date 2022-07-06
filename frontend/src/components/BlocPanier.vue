f33d4d0cbb5c59d1c3b47715f0a972212a827446
ChloeArnaud <64213782+ChloeArnaud@users.noreply.github.com>
2022-07-06 08:02:46 +0200

    Classes fonctionnalités du Panier

---
+++ frontend/src/components/BlocPanier.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <TitrePanier/>
+  <ItemPanier/>
+</div>
+</template>
+
+<script>
+import TitrePanier from "@/components/TitrePanier";
+import ItemPanier from "@/components/ItemPanier";
+export default {
+  name: "BlocPanier",
+  components: {ItemPanier, TitrePanier}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/TitrePanier.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <h3 class="titre">Mon panier</h3>
+  <ul id="example-1">
+    <li v-for="item in items" :key="item.message">
+      {{ item.message }}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+ export default {
+   name: "TitrePanier",
+   el: '#example-1',
+   data: function () {
+     return {
+       items: [
+         {message: 'Foo'},
+         {message: 'Bar'}
+       ]
+     }
+   },
+   methods : {
+
+   }
+ }
+</script>
+
+<style scoped>
+.titre{
+  background-color: #A4CBC3;
+  width: 100%;
+  padding: 2%;
+  margin: 2%;
+  color: floralwhite;
+}
+</style>
