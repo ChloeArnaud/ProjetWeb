e27410f665e76b68f28ed92cea12c7f58b487332
FarahFrgne05 <103503712+FarahFrgne05@users.noreply.github.com>
2022-07-07 09:28:15 +0200

    commit images

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -1,76 +1,66 @@
 <template>
   <div>
-    <div id="content " >
-      <div class="container">
-      <div class="row row-cols-4">
-        <div class="col" v-for="cat in this.lst_cat" id="blocBureau" v-bind:key="cat.idCategorie">
-           <!-- v-for="cat in this.lst_cat" -->
-           <!-- {{cat.NomCategorie}} -->
-          <BlocCategorie>
-            <router-link class="nav-link active" aria-current="page" to="/produitEcriture">{{cat.NomCategorie}}</router-link>
-          </BlocCategorie>
-       </div>
-      </div>
-      </div>
+    <div id="content">
+        <div class="row">
+           <div class="col-3" id="blocEcriture">
+              <BlocCategorie ><slot><router-link class="nav-link active" aria-current="page" to="/produitEcriture">Ecriture</router-link></slot></BlocCategorie>   
+           </div>
+          <div class="col-4" id="blocCahier">
+              <BlocCategorie ><slot><router-link class="nav-link active" aria-current="page" to="/produitCahier">Cahiers, carnets, feuilles</router-link></slot></BlocCategorie>
+          </div>
+          <div class="col-3" id="blocAgenda">
+             <BlocCategorie><slot><router-link class="nav-link active" aria-current="page" to="/produitAgenda">Agenda</router-link></slot></BlocCategorie>
+          </div>
 
+        </div>
+        <div class="row">
+          <div class="col-3" id="blocBureau">
+            <BlocCategorie><slot><router-link class="nav-link active" aria-current="page" to="/produitBureau">Bureau</router-link></slot></BlocCategorie>
+          </div>
+          <div class="col-4" id="blocAdhesif">
+            <BlocCategorie><slot><router-link class="nav-link active" aria-current="page" to="/produitAdhesif">Adhésifs</router-link></slot></BlocCategorie>
+          </div>
+          <div class="col-3" id="blocPeinture">
+            <BlocCategorie><slot><router-link class="nav-link active" aria-current="page" to="/produitPeinture">Peinture</router-link></slot></BlocCategorie>
+          </div>
+        </div>
     </div>
   </div>
+
 </template>
 
 <script>
 
-
+import ProduitAgenda from "@/components/Dropdown_folder/ProduitAgenda";
+import ProduitCahier from "@/components/Dropdown_folder/ProduitCahier";
+import ProduitBureau from "@/components/Dropdown_folder/ProduitBureau";
+import ProduitAdhesif from "@/components/Dropdown_folder/ProduitAdhesif";
+import ProduitPeinture from "@/components/Dropdown_folder/ProduitPeinture";
 import BlocCategorie from "@/components/BlocCategorie";
-import axios from 'axios'
-
-
-
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Shop",
-  data: function(){
-    return {
-        lst_cat : [],
-        cat : {},
-    }
-  },
-  beforeMount() {
-    this.lst_cat.axios
-    axios.get("http://127.0.0.1:3000/allcategories", function (req,res) {
-       res.header("Access-Control-Allow-Origin", "*");})
-        .then(result => {
-              console.log(this.lst_cat);
-              this.lst_cat = result.data;
-              this.cat = lst_cat[0];
-              console.log("CAT --> "+cat);
-        })
-        .catch(error => console.log(error));
-  },
   components:{
     BlocCategorie,
   }
 }
-
 
 </script>
 
 <style scoped>
 
 #blocEcriture{background-color: #EDAD9E; margin: 20px;}
-#blocCahier{background-color: #A4CBC3; margin: 20px;}
+#blocCahier{background-color: #A4CBC3; margin: 20px; }
 #blocAgenda{background-color: #E5BB70; margin: 20px;}
-#blocBureau{background-color: #599391; margin: 20px ;}
-#blocAdhesif{background-color: #E17F58; margin: 20px ;}
-#blocPeinture{background-color: #EDAD9E; margin: 20px ;}
+#blocBureau{background-color: #599391; margin: 20px;}
+#blocAdhesif{background-color: #E17F58; margin: 20px;}
+#blocPeinture{background-color: #EDAD9E; margin: 20px;}
 
-#content {
+.row {
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
   filter: drop-shadow(0.1rem 0.1rem 0.1rem #656565);
-  align-items: center;
-  justify-content: center;
   }
-
 
 </style>
